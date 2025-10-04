fcc4a6024e143e4e3fc0b2d85fbc495ff629f398
Ben A <195394081+swebena@users.noreply.github.com>
2025-10-04 12:44:05 +0000

    chore: basic game loop

---
+++ game.py
@@ -0,0 +1,32 @@
+import pygame
+from settings import WIDTH, HEIGHT, FPS
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.fps = FPS
+        self.running = True
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        # TODO: logic
+        pass
+
+    def draw(self):
+        self.screen.fill((30, 30, 30))
+        # TODO: draw game objects
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
+        pygame.quit()
